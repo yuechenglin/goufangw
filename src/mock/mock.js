e348d6f68e854cc23049bfc5a6457266fb462dd9
zhangdongzd <972757364@qq.com>
2017-04-01 08:55:13 +0800

    4-01

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,10 +1,7 @@
 var list = require('./list.json')
-var ershoufanglist = require('./ershoufanglist.json')
-var zufanglist = require('./zufanglist.json')
+
 module.exports = function() {
   return {
-    'list.php': list,
-    'ershoufanglist.php': ershoufanglist,
-    'zufanglist.php': zufanglist
+    'list.php': list
   }
 }
--- src/script/component/buy.js
+++ src/script/component/buy.js
@@ -1,10 +1,59 @@
 import React from 'react'
 
 class Buy extends React.Component {
+	constructor(props) {
+	    super(props)
+		 this.state = {
+	      Frist: [<div/>]    
+	    }
+	}
+	Add (){
+		console.info(this);
+		console.log("ok")
+	}
+	componentWillMount (){
+	 let msg = sessionStorage.getItem("userID")
+  	 let frist=()=>{}
+	    if(msg){
+	    	console.log(frist)
+      		frist = () => {
+      			return (
+	     				<div className="frist">
+	     					<div className="top">
+	     						<ul>
+	     							<li activeClassName="active" onClick={this.Add.bind(this)}>
+	     								<i className="yo-ico">&#xe6c1;</i>
+	     								<b>未评价</b>
+	     							</li>
+	     							<li activeClassName="active" onClick={this.Add.bind(this)}>
+	     								<i className="yo-ico">&#xe6c1;</i>
+	     								<b>已评价</b>
+	     							</li>
+	     						</ul>
+	     					</div>
+	     					<div className="bottom">下面没内容了</div>
+	     				</div>
+      			)	
+      		}		
+	    }else{
+    		 frist = ()=>{
+    			return (
+    				<div className="secend">
+    					<div>需要您登录才能使用</div>
+   					<input type="button" value="登录"/>
+    				</div>
+    			)
+    			
+    		}   		
+	    }
+	    this.setState({
+	       Frist:frist() 
+	    }) 
+	 }
   render() {
     return (
       <div className="m-buy">
-        Buy...
+	       	{this.state.Frist}
       </div>
     )
   }
